27d119e0ef5f9428aa908063c2ebd84e39f7c3c4
Artem Shipilov <shipilovrub@gmail.com>
2022-05-31 13:18:47 +0200

    Cosmos DB Support in Common

--- praktikum/shipilov/Microservices/Common/src/Common/Settings/MongoDbSettings.cs
+++ praktikum/shipilov/Microservices/Common/src/Common/Settings/MongoDbSettings.cs
@@ -2,10 +2,18 @@ namespace Common.Settings
 {
     public class MongoDbSettings
     {
+        private string connectionString;
         public string Host { get; init; }
 
         public int Port { get; init; }
 
-        public string ConnectionString => $"mongodb://{Host}:{Port}";
+        // public string ConnectionString => $"mongodb://{Host}:{Port}";
+
+        public string ConnectionString
+        {
+            get { return string.IsNullOrWhiteSpace(ConnectionString) ? $"mongodb://{Host}:{Port}" : connectionString; }
+            init { connectionString = value; }
+        }
+
     }
 }
